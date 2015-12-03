aaf24eb2d83ed03d8d52b382f81f19d32ce9bb3d
Mark <shuyumark@gmail.com>
2015-12-03 09:47:23 +0800

    update

--- chapter1/1.35.scm
+++ chapter1/1.35.scm
@@ -28,4 +28,37 @@
           (try next)))))
   (try first-guess))
 
-(print-fixed-point (lambda (x) (/ (log 1000) (log x))) 2.0)
+;(print-fixed-point (lambda (x) (/ (log 1000) (log x))) 2.0)
+
+
+;; 1.37
+
+(define (cont-frac n d k)
+  (define (cf n d y k)
+    (if (= y k)
+        (/ (n k) (d k))
+        (/ (n y) (+ (d y) (cf n d (+ y 1) k)))))
+  (cf n d 1 k))
+
+(define (cont-frac-iter n d k)
+  (define (iter n d k r)
+    (cond ((= k 1) (/ (n 1) (+ (d 1) r)))
+        (else (iter n d (- k 1) (/ (n k) (+ (d k) r))))))
+
+  (iter n d (- k 1) (/ (n k) (d k))))
+
+;; (/ 1.0 (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 100))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
