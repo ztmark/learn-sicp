bf2edd958e5e88ad1912e3e5f670cddb9a48bf6b
Mark <shuyumark@gmail.com>
2015-12-03 14:39:50 +0800

    update

--- chapter1/1.40.scm
+++ chapter1/1.40.scm
@@ -25,3 +25,84 @@
 
 (define (cubic a b c)
   (lambda (x) (+ (* x x x) (* a x x) (* b x) c)))
+
+
+;;; 1.41
+(define (double p)
+  (lambda (x) (p (p x))))
+
+;;;; 1.42
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+
+;;; 1.43
+(define (repeated f n)
+  (if (= n 1)
+      f
+      (lambda (x) (f ((repeated f (- n 1)) x)))))
+
+
+;;;;; 1.44
+(define (t-average a b c)
+  (/ (+ a b c) 3))
+(define (smooth f)
+  (lambda (x) (t-average (f (- x dx)) (f x) (f (+ x dx)))))
+
+
+(define (smooth-n f n)
+  (repeated (smooth f) n))
+
+;;;; 1.45
+(define (average-damp f)
+  (define (average x y)
+    (/ (+ x y) 2))
+  (lambda (x) (average x (f x))))
+
+(define (nf f)
+  (lambda (x) ((repeated average-damp 2) f) x))
+
+(define (nrt g guess)
+  (fixed-point (nf g) guess))
+
+
+
+;;;; 1.46
+(define (iterative-improve good-enough? improve-guess)
+  (lambda (guess) (if (good-enough? guess)
+      guess
+      ((iterative-improve good-enough? improve-guess) (improve-guess guess)))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
