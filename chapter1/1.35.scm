291592ff3f24d498b5d610cedba4c5b1787edff0
Mark <shuyumark@gmail.com>
2015-12-03 09:06:17 +0800

    update

--- chapter1/1.35.scm
+++ chapter1/1.35.scm
@@ -13,3 +13,16 @@
 
 
 ;(fixed-point (lambda (x) (+ (/ 1 x) 1)) 2.0)
+
+
+(define (print-fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (newline)
+    (display guess)
+    (let ((next (f guess)))
+      ((if (close-enough? guess next)
+          next
+          (try next)))))
+  (try first-guess))
