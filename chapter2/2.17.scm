8d5ddd43b943815eb945cd795ad66a617f4c64be
Mark <shuyumark@gmail.com>
2015-12-08 17:01:16 +0800

    update

--- chapter2/2.17.scm
+++ chapter2/2.17.scm
@@ -2,7 +2,8 @@
 ;; (cons 1 (list 2 3 4 5)) -> (1 2 3 4 5)
 ;; (cons (list 1 2 3 4) 5) -> ((1 2 3 4) 5)
 ;; (cons (list 1 2 3) (list 4 5)) -> ((1 2 3) 4 5)
-;; ?
+;; because (car (cons a b)) = a and car produce a s-expression
+;; (cdr (cons a b)) = b and cdr produce a list
 (define (append list1 list2)
   (if (null? list1)
       list2
