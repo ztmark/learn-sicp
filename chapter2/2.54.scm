e5fadf11186d82fe0b5d2ec52618fb43f16b413f
Mark <shuyumark@gmail.com>
2015-12-09 21:25:24 +0800

    update

--- chapter2/2.54.scm
+++ chapter2/2.54.scm
@@ -2,8 +2,10 @@
 
 (define (my-equal? items1 items2)
   (cond ((and (null? items1) (null? items2)) #t)
-      ((or (null? items1) (null? items2)) #f)
-      ((and (not (pair? items1)) (not (pair? items2))) (eq? items1 items2))
+      ;((or (null? items1) (null? items2)) #f)
+      ((and (symbol? items1) (symbol? items2)) (eq? items1 items2))
+      ((and (number? items1) (number? items2)) (= items1 items2))
+      ;((or (symbol? items1) (symbol? items2)) #f)
       ((and (pair? items1) (pair? items2)) 
         (and (eq? (car items1) (car items2)) (my-equal? (cdr items1) (cdr items2))))
       (else #f)))
